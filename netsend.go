01ad711af587d67462c7ed940878e8a119ad0dd6
Sam <sam.mackenzie.nmit@gmail.com>
2018-03-09 18:56:40 +1300

    Refactor modes into diffrent functions

--- netsend.go
+++ netsend.go
@@ -22,35 +22,49 @@ func listenerToPort(l *net.Listener) int {
 	return (*l).Addr().(*net.TCPAddr).Port
 }
 
+func serveWeb(port int) {
+	pipe, _ := ioutil.ReadAll(os.Stdin)
+	sPipe := string(pipe)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, sPipe)
+	})
+	l, _ := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+	fmt.Println(listenerToPort(&l))
+	log.Fatal(http.Serve(l, nil))
+
+}
+
+func sendTCP(addr string) {
+
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal("Failed to connect.")
+	} else {
+		io.Copy(c, os.Stdin)
+		c.Close()
+		log.Printf("Sent.")
+	}
+}
+
+func receveTCP(port int) {
+	l, _ := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+	c, _ := l.Accept()
+	b, _ := ioutil.ReadAll(c)
+	fmt.Print(string(b))
+}
+
 func main() {
 
 	flag.Parse()
 	if *listen != 0 { //TCP Receving
-		l, _ := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *listen))
-		c, _ := l.Accept()
-		b, _ := ioutil.ReadAll(c)
-		fmt.Print(string(b))
+		receveTCP(*listen)
 	} else if *web != 0 { //WEB Receving
-		pipe, _ := ioutil.ReadAll(os.Stdin)
-		sPipe := string(pipe)
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, sPipe)
-		})
-		l, _ := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *web))
-		fmt.Println(listenerToPort(&l))
-		log.Fatal(http.Serve(l, nil))
+		serveWeb(*web)
 	} else if *send != "" { //Sending
 		lAddr := *send
 		if !strings.Contains(lAddr, ":") {
 			lAddr = fmt.Sprintf("127.0.0.1:%s", lAddr)
 		}
-		c, err := net.Dial("tcp", lAddr)
-		if err != nil {
-			log.Fatal("Failed to connect.")
-		} else {
-			io.Copy(c, os.Stdin)
-			c.Close()
-			log.Printf("Sent.")
-		}
+		sendTCP(lAddr)
 	}
 }
